8d616eb2f7e50aa64262f65f4258fbf504e31721
slne225 <94694817+slne225@users.noreply.github.com>
2022-11-10 21:58:20 -0500

    inline-flex demo added

--- responsive.js
+++ responsive.js
@@ -1,11 +1,35 @@
-let flex = document.getElementById('flex');
-let button = document.getElementById('flex-button')
-
+//toggle diplay between flex and block
+const flex = document.getElementById('flex');
+const flexbutton = document.getElementById('flexbutton');
 
 function blockDisplay() {
     if (flex.style.display === 'flex') {
         flex.style.display = 'block';
-    } else { flex.style.display = 'flex'}
+        flexbutton.innerHTML = 'display: block';
+    } else { 
+        flex.style.display = 'flex';
+        flexbutton.innerHTML = 'display: flex'
+}
 }
 
-flex-button.addEventListener('click', blockDisplay)
+flexbutton.addEventListener('click', blockDisplay);
+
+//toggle display between inline-flex and block
+const inlineflex = document.getElementsByClassName('inline-flex container')
+const inlinebutton = document.getElementById('inlinebutton')
+
+function inlineFlexDisplay() {
+    for (let i = 0; i < inlineflex.length; i++){
+        if (inlineflex[i].style.display === 'block') {
+            inlineflex[i].style.display = 'inline-flex'
+            inlinebutton.innerHTML = 'display: inline-flex'
+        }
+        else {
+            inlineflex[i].style.display = 'block';
+            inlinebutton.innerHTML = 'display: block'
+        }
+    }
+}
+
+
+inlinebutton.addEventListener('click', inlineFlexDisplay)
